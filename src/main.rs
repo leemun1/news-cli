c3e39efcf01c54f23b20b1159ae45db57652d354
Mike Lee <mikemunkyu.lee@gmail.com>
2022-08-15 09:37:23 -0400

    Serialize API response

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ extern crate dotenv;
 use dotenv::dotenv;
 use std::env;
 
+use serde::Deserialize;
 
+#[derive(Deserialize, Debug)]
 struct Articles  {
     articles: Vec<Article>,
 }
 
+#[derive(Deserialize, Debug)]
 struct Article {
     title: String,
     url: String,
@@ -17,16 +20,16 @@ struct Article {
 fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>> {
     let response = ureq::get(url).call()?.into_string()?;
 
-    println!("yooo {}", response);
+    let articles: Articles = serde_json::from_str(&response)?;
 
-    dbg!(response);
-
-    todo!()
+    Ok(articles)
 }
 fn main() {
     dotenv().ok();
 
-    let api_key = env::var("API_KEY");
-    let url= format!("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey={:?}", api_key);
+    let api_key = env::var("API_KEY").unwrap();
+    let url= format!("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey={}", api_key);
     let articles = get_articles(&url);
+
+    dbg!(articles);
 }
